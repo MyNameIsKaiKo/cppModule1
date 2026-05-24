77cd95175f1bd0839c4e2e7d384132b605793e03
Jean-Baptiste Leray <jleray@1B6.42angouleme.fr>
2026-05-24 18:38:18 +0200

    ex05 is done

---
+++ ex05/src/Harl.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleray <marvin@d42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/24 18:01:43 by jleray            #+#    #+#             */
+/*   Updated: 2026/05/24 18:01:43 by jleray           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void	Harl::debug()
+{
+	std::cout << "I love having extra bacon for my ";
+	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	return ;
+}
+
+void	Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	return ;
+}
+
+void	Harl::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	return ;
+}
+
+void	Harl::error()
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.";
+	return ;
+}
+
+void	Harl::complain(std::string level)
+{
+	void (Harl::*funcs[4]) (void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int	i = 0;
+
+	while (i < 4)
+	{
+		if (level == levels[i])
+		{
+			(this->*funcs[i])();
+		}
+		i++;
+	}
+}
